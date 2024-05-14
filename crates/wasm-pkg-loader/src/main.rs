26a13cd7a6a3da13f1573291590ae7a102b8c73d
Calvin Prewitt <calvin@jaflabs.com>
2024-05-14 10:06:15 -0500

    reverted config

--- crates/wasm-pkg-loader/src/main.rs
+++ crates/wasm-pkg-loader/src/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use anyhow::{anyhow, bail, ensure, Context};
+use anyhow::{bail, ensure, Context};
 use futures_util::TryStreamExt;
 use tokio::io::AsyncWriteExt;
 use wasm_pkg_loader::{Client, ClientConfig, PackageRef, Release, Version};
@@ -22,29 +22,11 @@ async fn main() -> anyhow::Result<()> {
     };
 
     let client = {
-        let config = ClientConfig::from_default_file()?;
-        let is_missing_config = config.is_none();
-        let mut config = config.unwrap_or_default();
-
-        // if default registry is not configured, fallback to Warg home URL
-        if config.default_registry().is_none() {
-            match warg_client::Config::from_default_file()? {
-                Some(warg_client::Config { home_url, .. }) if home_url.is_some() => {
-                    let url = url::Url::parse(&home_url.unwrap())?;
-                    config.set_default_registry(
-                        url.host_str()
-                            .ok_or_else(|| anyhow!("invalid Warg home url"))?,
-                    );
-                }
-                _ => {}
-            };
+        let mut config = ClientConfig::default();
+        config.set_namespace_registry("wasi", "bytecodealliance.org");
+        if let Some(file_config) = ClientConfig::from_default_file()? {
+            config.merge_config(file_config);
         }
-
-        // if not configured and not using Warg home URL as default registry,
-        // then set the `wasi` default registry
-        if is_missing_config && config.default_registry().is_none() {
-            config.set_namespace_registry("wasi", "bytecodealliance.org");
-        };
 
         config.to_client()
     };
