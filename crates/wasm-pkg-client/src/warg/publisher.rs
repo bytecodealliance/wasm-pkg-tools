75d76192c30e718d5b76123720c79153083ed3e6
Taylor Thomas <taylor@cosmonic.com>
2024-09-04 11:05:09 -0600

    fix(warg): Makes sure to init the package if it isn't available yet
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

--- crates/wasm-pkg-client/src/warg/publisher.rs
+++ crates/wasm-pkg-client/src/warg/publisher.rs
@@ -39,10 +39,19 @@ impl PackagePublisher for WargBackend {
 
         // start Warg publish, using the keyring to sign
         let version = version.clone();
+        // Check if the package already exists so we can init it if needed
+        let release = PublishEntry::Release { version, content };
+        let entries = if let Err(warg_client::ClientError::PackageDoesNotExist { .. }) =
+            self.client.fetch_package(&name).await
+        {
+            vec![PublishEntry::Init, release]
+        } else {
+            vec![release]
+        };
         let info = PublishInfo {
             name: name.clone(),
             head: None,
-            entries: vec![PublishEntry::Release { version, content }],
+            entries,
         };
         let record_id = if let Some(key) = self.signing_key.as_ref() {
             self.client.publish_with_info(key, info).await
